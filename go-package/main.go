151d2022f0bfbebfdb5c2e11731a3a16a4426987
Puneet Blockchain <Puneet-blockchain@tarality.com>
2023-12-02 18:06:00 +0530

    we are using go-ethereum code base here

--- go-package/main.go
+++ go-package/main.go
@@ -7,6 +7,8 @@ import (
 	"go-ethereum/package-tutorial/package_2"
 	"go-ethereum/package-tutorial/package_3"
 
+	"github.com/ethereum/go-ethereum/core/types"
+
 	"fmt"
 )
 
@@ -16,6 +18,6 @@ func main() {
 	package_1.Function_1()
 	package_2.Function_2()
 	package_3.Function_3()
-
+	types.EncodeNonce(5) // this is how use go-ethereum code's function and packages
 	fmt.Println("--------end------")
 }
